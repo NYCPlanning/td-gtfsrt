ed1cce95d3ffcca4e29e61bb97a02f3395006f81
mayijun1203 <mayijun1203@gmail.com>
2019-07-18 16:53:44 -0400

    Update download.py

--- download.py
+++ download.py
@@ -1,21 +1,25 @@
 import requests
 import zipfile
+import os
 
 
 #path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2019/GTFS-RT/'
 #path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
 path='/home/mayijun/GTFS-RT/'
 
-url='https://s3.amazonaws.com/gtfsarchive/Data/201905.zip'
+#url='https://s3.amazonaws.com/gtfsarchive/Data/201905.zip'
+#with requests.get(url, stream=True) as r:
+#    r.raise_for_status()
+#    with open(path+'201905.zip', 'wb') as f:
+#        for chunk in r.iter_content(chunk_size=100): 
+#            if chunk: # filter out keep-alive new chunks
+#                f.write(chunk)
+#
+#zip_ref=zipfile.ZipFile(path+'201905.zip','r')
+#zip_ref.extractall(path+'201905')
+#zip_ref.close()
 
-with requests.get(url, stream=True) as r:
-    r.raise_for_status()
-    with open(path+'201905.zip', 'wb') as f:
-        for chunk in r.iter_content(chunk_size=100): 
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-
-zip_ref=zipfile.ZipFile(path+'201905.zip','r')
-zip_ref.extractall(path+'201905')
-zip_ref.close()
+for i in os.listdir(path+'201905/'):
+    zip_ref=zipfile.ZipFile(path+'201905/'+str(i),'r')
+    zip_ref.extractall(path+'201905/'+str(i.replace('.zip','')))
+    zip_ref.close()
