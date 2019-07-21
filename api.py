28f3412735494c834a41a56dbdfa7794c1f1aede
mayijun1203 <mayijun1203@gmail.com>
2019-07-20 21:28:47 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,33 +6,15 @@ import time
 import numpy as np
 import requests
 
-start=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+
 pd.set_option('display.max_columns', None)
-#path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2019/GTFS-RT/'
-path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
+path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2019/GTFS-RT/'
+#path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
 #path='/home/mayijun/GTFS-RT/'
 stops=pd.read_csv(path+'Schedule/stops.txt')
 key='9aea332316c4d6c5332aecc2a733033d'
-
-
-
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('http://datamine.mta.info/mta_esi.php?key='+str(key)+'&feed_id=51')
-feed.ParseFromString(response.content)
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
-
-
-
-
-
-
-
-
-
-
+fds=['1','26','16','21','2','11','31','36','51']
 
 
 
@@ -52,6 +34,8 @@ def calduration(df):
 
 
 def cleangtfsrt(fd):
+    realtime=[]
+    schedule=[]
     for f in fd:
         try:
             feed=gtfs_realtime_pb2.FeedMessage()
@@ -67,7 +51,7 @@ def cleangtfsrt(fd):
                         rt['routeid']=[entity.trip_update.trip.route_id]
                         rt['tripid']=[entity.trip_update.trip.trip_id]
                         rt=rt.dropna()
-                        rt.to_csv(path+'Output/rttp.csv',index=False,header=False,mode='a')
+                        realtime.append(rt)
                         # schedule
                         sc=pd.DataFrame(columns=['routeid','tripid','stopid','time'])
                         sc['stopid']=[x.stop_id for x in entity.trip_update.stop_time_update[1:]]
@@ -77,83 +61,76 @@ def cleangtfsrt(fd):
                         sc=sc.dropna()
                         sc=sc.sort_values(['routeid','tripid','time']).reset_index(drop=True)
                         sc=calduration(sc)
-                        sc.to_csv(path+'Output/sctp.csv',index=False,header=False,mode='a')
+                        schedule.append(sc)
                     except:
                         print(str(f)+' entity error')
             response.close()
         except:
             print(str(f)+' response error')
+    realtime=pd.concat(realtime,axis=0,ignore_index=True)
+    schedule=pd.concat(schedule,axis=0,ignore_index=True)
+    return realtime,schedule
 
 
 
 def parallelize(data, func):
     data_split = np.array_split(data,mp.cpu_count()-1)
     pool = mp.Pool(mp.cpu_count()-1)
-    pool.map(func, data_split)
+    res=pool.map(func, data_split)
+    a=pd.concat([x[0] for x in res],axis=0,ignore_index=True)
+    b=pd.concat([x[1] for x in res],axis=0,ignore_index=True)
     pool.close()
     pool.join()
-    return data
+    return a,b
 
 
 
 if __name__=='__main__':
-    endtime=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime('2019-07-19 17:10:00'))
-    while time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())<)
-    rttp=pd.DataFrame(columns=['routeid','tripid','stopid','time'])
-    rttp.to_csv(path+'Output/rttp.csv',index=False,header=True,mode='w')
-    sctp=pd.DataFrame(columns=['routeid','tripid','starthour','startstopid','starttime','endstopid','endtime','duration'])
-    sctp.to_csv(path+'Output/sctp.csv',index=False,header=True,mode='w')
-    fds=['1','26','16','21','2','11','31','36','51']
-    parallelize(fds, cleangtfsrt)
+    endtime=time.strptime('2019-07-20 21:45:00','%Y-%m-%d %H:%M:%S')
+    while time.localtime()<endtime:
+        starttime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+        rttp,sctp=parallelize(fds, cleangtfsrt)
+        rttp.to_csv(path+starttime[0:7].replace('-','')+'/'+starttime[0:10].replace('-','')+'/'+'rttp_'+
+                    starttime.replace('-','').replace(':','').replace(' ','_')+'.csv',index=False,header=True,mode='w')
+        sctp.to_csv(path+starttime[0:7].replace('-','')+'/'+starttime[0:10].replace('-','')+'/'+'sctp_'+
+                    starttime.replace('-','').replace(':','').replace(' ','_')+'.csv',index=False,header=True,mode='w')
+        time.sleep(10)
+        
 
-    for i in fds:
-
-        for m in months:
-            dates=sorted(os.listdir(path+m+'/'))
-            for d in dates:
-                routes=sorted(pd.unique([x.split('_')[1] for x in os.listdir(path+str(m)+'/'+str(d)+'/')]))
-                for r in routes:
-                    rttp=pd.DataFrame(columns=['routeid','tripid','stopid','time'])
-                    rttp.to_csv(path+'Output/rttp.csv',index=False,header=True,mode='w')
-                    sctp=pd.DataFrame(columns=['routeid','tripid','starthour','startstopid','starttime','endstopid','endtime','duration'])
-                    sctp.to_csv(path+'Output/sctp.csv',index=False,header=True,mode='w')
-                    feed = gtfs_realtime_pb2.FeedMessage()
-                    files=sorted([x for x in os.listdir(path+str(m)+'/'+str(d)+'/') if x.startswith('gtfs_'+str(r)+'_'+str(d))])
-                    parallelize(files, cleangtfsrt)
-                    rttp=pd.read_csv(path+'Output/rttp.csv',dtype=str)
-                    rttp['time']=pd.to_numeric(rttp['time'])
-                    rttp=rttp.groupby(['routeid','tripid','stopid'],as_index=False).agg({'time':'median'})
-                    rttp=rttp.sort_values(['routeid','tripid','time']).reset_index(drop=True)
-                    rttp=rttp.groupby(['routeid','tripid'],as_index=False).apply(calduration).reset_index(drop=True)
-                    sctp=pd.read_csv(path+'Output/sctp.csv',dtype=str)
-                    sctp['duration']=pd.to_numeric(sctp['duration'])
-                    sctp=sctp.groupby(['routeid','tripid','startstopid','endstopid'],as_index=False).agg({'duration':'median'})
-                    sctp.columns=['routeid','tripid','startstopid','endstopid','schedule']
-                    tp=pd.merge(rttp,sctp,how='left',on=['routeid','tripid','startstopid','endstopid'])
-                    tp=tp.dropna()
-                    tp['delay']=tp.duration-tp.schedule
-                    tp['delaypct']=tp.duration/tp.schedule
-                    tp=pd.merge(tp,stops[['stop_id','stop_name']],how='left',left_on='startstopid',right_on='stop_id')
-                    tp=pd.merge(tp,stops[['stop_id','stop_name']],how='left',left_on='endstopid',right_on='stop_id')
-                    tp=tp[['routeid','tripid','starthour','startstopid','stop_name_x','starttime',
-                           'endstopid','stop_name_y','endtime','duration','schedule','delay','delaypct']]
-                    tp.columns=['routeid','tripid','starthour','startstopid','startstopname','starttime',
-                                'endstopid','endstopname','endtime','duration','schedule','delay','delaypct']
-                    tp.to_csv(path+'Output/'+str(d)+'_'+str(r)+'.csv',index=False,header=True,mode='w')
-            print(datetime.datetime.now()-start)
-        #    tp=pd.DataFrame()
-        #    for i in os.listdir(path+'Output/'):
-        #        tp=tp.append(pd.read_csv(path+'Output/'+str(i),dtype=str))
-        #    tp['duration']=pd.to_numeric(tp['duration'])
-        #    tp['schedule']=pd.to_numeric(tp['schedule'])
-        #    tp['delay']=pd.to_numeric(tp['delay'])
-        #    tp['delaypct']=pd.to_numeric(tp['delaypct'])
-        #    tp=tp[tp.starthour.isin(['06','07','08','09'])]
-        #    tp=tp.groupby(['routeid','startstopid','endstopid'],as_index=False).agg({'duration':['min','median','mean','max','count'],
-        #                 'schedule':['min','median','mean','max','count'],'delaypct':['min','median','mean','max','count']})
-        #    tp.columns=[x[0]+x[1] for x in tp.columns]
-        #    tp=tp.groupby(['starthour'],as_index=False).agg({'delaypct':['min','median','mean','max','count']})
-        #    tp.columns=[x[0]+x[1] for x in tp.columns]
+    
+#    rttp['time']=pd.to_numeric(rttp['time'])
+#    rttp=rttp.groupby(['routeid','tripid','stopid'],as_index=False).agg({'time':'median'})
+#    rttp=rttp.sort_values(['routeid','tripid','time']).reset_index(drop=True)
+#    rttp=rttp.groupby(['routeid','tripid'],as_index=False).apply(calduration).reset_index(drop=True)
+#    sctp['duration']=pd.to_numeric(sctp['duration'])
+#    sctp=sctp.groupby(['routeid','tripid','startstopid','endstopid'],as_index=False).agg({'duration':'median'})
+#    sctp.columns=['routeid','tripid','startstopid','endstopid','schedule']
+#    
+#    tp=pd.merge(rttp,sctp,how='left',on=['routeid','tripid','startstopid','endstopid'])
+#    tp=tp.dropna()
+#    tp['delay']=tp.duration-tp.schedule
+#    tp['delaypct']=tp.duration/tp.schedule
+#    tp=pd.merge(tp,stops[['stop_id','stop_name']],how='left',left_on='startstopid',right_on='stop_id')
+#    tp=pd.merge(tp,stops[['stop_id','stop_name']],how='left',left_on='endstopid',right_on='stop_id')
+#    tp=tp[['routeid','tripid','starthour','startstopid','stop_name_x','starttime',
+#           'endstopid','stop_name_y','endtime','duration','schedule','delay','delaypct']]
+#    tp.columns=['routeid','tripid','starthour','startstopid','startstopname','starttime',
+#                'endstopid','endstopname','endtime','duration','schedule','delay','delaypct']
+#    tp.to_csv(path+'Output/'+str(d)+'_'+str(r)+'.csv',index=False,header=True,mode='w')
+#    print(datetime.datetime.now()-start)
+#    tp=pd.DataFrame()
+#    for i in os.listdir(path+'Output/'):
+#        tp=tp.append(pd.read_csv(path+'Output/'+str(i),dtype=str))
+#    tp['duration']=pd.to_numeric(tp['duration'])
+#    tp['schedule']=pd.to_numeric(tp['schedule'])
+#    tp['delay']=pd.to_numeric(tp['delay'])
+#    tp['delaypct']=pd.to_numeric(tp['delaypct'])
+#    tp=tp[tp.starthour.isin(['06','07','08','09'])]
+#    tp=tp.groupby(['routeid','startstopid','endstopid'],as_index=False).agg({'duration':['min','median','mean','max','count'],
+#                 'schedule':['min','median','mean','max','count'],'delaypct':['min','median','mean','max','count']})
+#    tp.columns=[x[0]+x[1] for x in tp.columns]
+#    tp=tp.groupby(['starthour'],as_index=False).agg({'delaypct':['min','median','mean','max','count']})
+#    tp.columns=[x[0]+x[1] for x in tp.columns]
 
 
 
