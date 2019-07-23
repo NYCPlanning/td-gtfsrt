24a8c0a8951c5ff6cff51261dbceb0e3b231fe90
mayijun1203 <mayijun1203@gmail.com>
2019-07-23 14:08:16 -0400

    Update archivedownload.py

--- archivedownload.py
+++ archivedownload.py
@@ -4,25 +4,25 @@ import os
 
 
 #path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2019/GTFS-RT/'
-path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
-#path='/home/mayijun/GTFS-RT/'
+#path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
+path='/home/mayijun/GTFS-RT/'
 
 months=['201906']
 for m in months:
-    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
-            for chunk in r.iter_content(chunk_size=100): 
-                if chunk: # filter out keep-alive new chunks
-                    f.write(chunk)
-    
+#    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
+#    with requests.get(url, stream=True) as r:
+#        r.raise_for_status()
+#        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
+#            for chunk in r.iter_content(chunk_size=100): 
+#                if chunk: # filter out keep-alive new chunks
+#                    f.write(chunk)
+#    
     zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'.zip','r')
-    zip_ref.extractall(path+str(m))
+    zip_ref.extractall(path+'Archive/'+str(m))
     zip_ref.close()
     
-#    for i in sorted(os.listdir(path+'Archive/'+str(m))):
-#        if i.endswith('.zip'):
-#            zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'/'+str(i),'r')
-#            zip_ref.extractall(path+'Archive/'+str(m)+'/'+str(i.replace('.zip','')))
-#            zip_ref.close()
+    for i in sorted(os.listdir(path+'Archive/'+str(m))):
+        if i.endswith('.zip'):
+            zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'/'+str(i),'r')
+            zip_ref.extractall(path+'Archive/'+str(m)+'/'+str(i.replace('.zip','')))
+            zip_ref.close()
