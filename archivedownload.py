a7c5478220529d89778c67324c9d0105c38ac3a1
mayijun1203 <mayijun1203@gmail.com>
2019-07-23 17:36:51 -0400

    Update archivedownload.py

--- archivedownload.py
+++ archivedownload.py
@@ -10,19 +10,18 @@ path='/home/mayijun/GTFS-RT/'
 
 
 
-#months=['201808','201809','201810','201811','201812','201901','201902','201903','201904']
-months=['201808','201809']
+months=['201810','201811','201812','201901','201902','201903','201904']
 for m in months:
-#    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
-#    with requests.get(url, stream=True) as r:
-#        r.raise_for_status()
-#        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
-#            for chunk in r.iter_content(chunk_size=100): 
-#                if chunk: # filter out keep-alive new chunks
-#                    f.write(chunk)
-#    zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'.zip','r')
-#    zip_ref.extractall(path+'Archive/'+str(m))
-#    zip_ref.close()
+    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=100): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+    zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'.zip','r')
+    zip_ref.extractall(path+'Archive/'+str(m))
+    zip_ref.close()
     for i in sorted(os.listdir(path+'Archive/'+str(m))):
         if i.endswith('.zip'):
             zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'/'+str(i),'r')
