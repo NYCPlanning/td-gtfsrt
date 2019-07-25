85e149899861b9f70573e08481f270f4ae2cbc57
mayijun1203 <mayijun1203@gmail.com>
2019-07-25 11:19:06 -0400

    update

--- archivedownload.py
+++ archivedownload.py
@@ -8,17 +8,18 @@ import os
 #path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
 path='/home/mayijun/GTFS-RT/'
 
+# Unzip in linux 7z x
 
 
 months=['201810']
 for m in months:
-#    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
-#    with requests.get(url, stream=True) as r:
-#        r.raise_for_status()
-#        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
-#            for chunk in r.iter_content(chunk_size=100): 
-#                if chunk: # filter out keep-alive new chunks
-#                    f.write(chunk)
+    url='https://s3.amazonaws.com/gtfsarchive/Data/'+str(m)+'.zip'
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(path+'Archive/'+str(m)+'.zip', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=100): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
 #    zip_ref=zipfile.ZipFile(path+'Archive/'+str(m)+'.zip',mode='r')
 #    zip_ref.extractall(path+'Archive/'+str(m))
 #    zip_ref.close()
