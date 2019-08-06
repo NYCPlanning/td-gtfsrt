41551ec810f142a07b4461d4344bcfc8ae1cd5f4
mayijun1203 <mayijun1203@gmail.com>
2019-08-05 20:01:17 -0400

    Update archivesummary.py

--- archivesummary.py
+++ archivesummary.py
@@ -9,8 +9,8 @@ import geopandas as gpd
 
 pd.set_option('display.max_columns', None)
 #path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2019/GTFS-RT/'
-path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
-#path='/home/mayijun/GTFS-RT/'
+#path='C:/Users/Y_Ma2/Desktop/GTFS-RT/'
+path='/home/mayijun/GTFS-RT/'
 #path='E:GTFS-RT/'
 stops=pd.read_csv(path+'Schedule/stops.txt',dtype=str)
 routes=pd.read_csv(path+'Schedule/routes.txt',dtype=str)
