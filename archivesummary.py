fedc46566181875eb85b51d79112069ec2a26e60
mayijun1203 <mayijun1203@gmail.com>
2019-08-09 18:25:59 -0400

    Update archivesummary.py

--- archivesummary.py
+++ archivesummary.py
@@ -81,55 +81,55 @@ def calwaittime(wt):
 
 
 
-tp=[]
-for i in sorted([x for x in os.listdir(path+'Output/Archive/') if x.startswith('tp')]):
-    tp.append(pd.read_csv(path+'Output/Archive/'+str(i),dtype=str))
-tp=pd.concat(tp,axis=0,ignore_index=True)
-tp['duration']=pd.to_numeric(tp['duration'])
-tp['schedule']=pd.to_numeric(tp['schedule'])
-tp['delay']=pd.to_numeric(tp['delay'])
-tp['delaypct']=pd.to_numeric(tp['delaypct'])
-tp['startweekday']=[time.strptime(x,'%Y-%m-%d %H:%M:%S').tm_wday for x in tp['starttime']]
-tp=tp.groupby(['routeid','startstopid','endstopid'],as_index=False).apply(calwaittime).reset_index(drop=True)
-tp=tp[tp['startweekday'].isin([0,1,2,3,4])]
-tp=tp[tp['starthour'].isin(['06','07','08','09'])]
-tp=tp[['routeid','startstopid','endstopid','waittime','duration','schedule','delay','delaypct']]
-tp=tp.groupby(['routeid','startstopid','endstopid']).describe(percentiles=[0.1,0.25,0.5,0.75,0.9]).reset_index()
-tp.columns=[(x[0]+x[1]).replace('%','') for x in tp.columns]
-tp['waittimeqcv']=(tp['waittime75']-tp['waittime25'])/tp['waittime50']
-tp['durationqcv']=(tp['duration75']-tp['duration25'])/tp['duration50']
-tp['scheduleqcv']=(tp['schedule75']-tp['schedule25'])/tp['schedule50']
-tp['delayqcv']=(tp['delay75']-tp['delay25'])/tp['delay50']
-tp['delaypctqcv']=(tp['delaypct75']-tp['delaypct25'])/tp['delaypct50']
-tp=tp[tp['durationcount']>100]
-tp=pd.merge(tp,routes,how='left',left_on='routeid',right_on='route_id')
-tp=pd.merge(tp,stops[['stop_id','stop_name','stop_lat','stop_lon']],how='left',left_on='startstopid',right_on='stop_id')
-tp=pd.merge(tp,stops[['stop_id','stop_name','stop_lat','stop_lon']],how='left',left_on='endstopid',right_on='stop_id')
-tp=tp[['routeid','route_color','startstopid','stop_name_x','stop_lat_x','stop_lon_x',
-       'endstopid','stop_name_y','stop_lat_y','stop_lon_y',
-       'waittimecount','waittimemin','waittimemax','waittimemean','waittimestd',
-       'waittime10','waittime25','waittime50','waittime75','waittime90','waittimeqcv',
-       'durationcount','durationmin','durationmax','durationmean','durationstd',
-       'duration10','duration25','duration50','duration75','duration90','durationqcv',
-       'schedulecount','schedulemin','schedulemax','schedulemean','schedulestd',
-       'schedule10','schedule25','schedule50','schedule75','schedule90','scheduleqcv',
-       'delaycount','delaymin','delaymax','delaymean','delaystd',
-       'delay10','delay25','delay50','delay75','delay90','delayqcv',
-       'delaypctcount','delaypctmin','delaypctmax','delaypctmean','delaypctstd',
-       'delaypct10','delaypct25','delaypct50','delaypct75','delaypct90','delaypctqcv']]
-tp.columns=['routeid','routecolor','startstopid','startstopname','startstoplat','startstoplong',
-            'endstopid','endstopname','endstoplat','endstoplong',
-            'waittimecount','waittimemin','waittimemax','waittimemean','waittimestd',
-            'waittime10','waittime25','waittime50','waittime75','waittime90','waittimeqcv',
-            'durationcount','durationmin','durationmax','durationmean','durationstd',
-            'duration10','duration25','duration50','duration75','duration90','durationqcv',
-            'schedulecount','schedulemin','schedulemax','schedulemean','schedulestd',
-            'schedule10','schedule25','schedule50','schedule75','schedule90','scheduleqcv',
-            'delaycount','delaymin','delaymax','delaymean','delaystd',
-            'delay10','delay25','delay50','delay75','delay90','delayqcv',
-            'delaypctcount','delaypctmin','delaypctmax','delaypctmean','delaypctstd',
-            'delaypct10','delaypct25','delaypct50','delaypct75','delaypct90','delaypctqcv']
-tp.to_csv(path+'Output/Archive/ArchiveOutput.csv',index=False,header=True,mode='w')
+#tp=[]
+#for i in sorted([x for x in os.listdir(path+'Output/Archive/') if x.startswith('tp')]):
+#    tp.append(pd.read_csv(path+'Output/Archive/'+str(i),dtype=str))
+#tp=pd.concat(tp,axis=0,ignore_index=True)
+#tp['duration']=pd.to_numeric(tp['duration'])
+#tp['schedule']=pd.to_numeric(tp['schedule'])
+#tp['delay']=pd.to_numeric(tp['delay'])
+#tp['delaypct']=pd.to_numeric(tp['delaypct'])
+#tp['startweekday']=[time.strptime(x,'%Y-%m-%d %H:%M:%S').tm_wday for x in tp['starttime']]
+#tp=tp.groupby(['routeid','startstopid','endstopid'],as_index=False).apply(calwaittime).reset_index(drop=True)
+#tp=tp[tp['startweekday'].isin([0,1,2,3,4])]
+#tp=tp[tp['starthour'].isin(['06','07','08','09'])]
+#tp=tp[['routeid','startstopid','endstopid','waittime','duration','schedule','delay','delaypct']]
+#tp=tp.groupby(['routeid','startstopid','endstopid']).describe(percentiles=[0.1,0.25,0.5,0.75,0.9]).reset_index()
+#tp.columns=[(x[0]+x[1]).replace('%','') for x in tp.columns]
+#tp['waittimeqcv']=(tp['waittime75']-tp['waittime25'])/tp['waittime50']
+#tp['durationqcv']=(tp['duration75']-tp['duration25'])/tp['duration50']
+#tp['scheduleqcv']=(tp['schedule75']-tp['schedule25'])/tp['schedule50']
+#tp['delayqcv']=(tp['delay75']-tp['delay25'])/tp['delay50']
+#tp['delaypctqcv']=(tp['delaypct75']-tp['delaypct25'])/tp['delaypct50']
+#tp=tp[tp['durationcount']>100]
+#tp=pd.merge(tp,routes,how='left',left_on='routeid',right_on='route_id')
+#tp=pd.merge(tp,stops[['stop_id','stop_name','stop_lat','stop_lon']],how='left',left_on='startstopid',right_on='stop_id')
+#tp=pd.merge(tp,stops[['stop_id','stop_name','stop_lat','stop_lon']],how='left',left_on='endstopid',right_on='stop_id')
+#tp=tp[['routeid','route_color','startstopid','stop_name_x','stop_lat_x','stop_lon_x',
+#       'endstopid','stop_name_y','stop_lat_y','stop_lon_y',
+#       'waittimecount','waittimemin','waittimemax','waittimemean','waittimestd',
+#       'waittime10','waittime25','waittime50','waittime75','waittime90','waittimeqcv',
+#       'durationcount','durationmin','durationmax','durationmean','durationstd',
+#       'duration10','duration25','duration50','duration75','duration90','durationqcv',
+#       'schedulecount','schedulemin','schedulemax','schedulemean','schedulestd',
+#       'schedule10','schedule25','schedule50','schedule75','schedule90','scheduleqcv',
+#       'delaycount','delaymin','delaymax','delaymean','delaystd',
+#       'delay10','delay25','delay50','delay75','delay90','delayqcv',
+#       'delaypctcount','delaypctmin','delaypctmax','delaypctmean','delaypctstd',
+#       'delaypct10','delaypct25','delaypct50','delaypct75','delaypct90','delaypctqcv']]
+#tp.columns=['routeid','routecolor','startstopid','startstopname','startstoplat','startstoplong',
+#            'endstopid','endstopname','endstoplat','endstoplong',
+#            'waittimecount','waittimemin','waittimemax','waittimemean','waittimestd',
+#            'waittime10','waittime25','waittime50','waittime75','waittime90','waittimeqcv',
+#            'durationcount','durationmin','durationmax','durationmean','durationstd',
+#            'duration10','duration25','duration50','duration75','duration90','durationqcv',
+#            'schedulecount','schedulemin','schedulemax','schedulemean','schedulestd',
+#            'schedule10','schedule25','schedule50','schedule75','schedule90','scheduleqcv',
+#            'delaycount','delaymin','delaymax','delaymean','delaystd',
+#            'delay10','delay25','delay50','delay75','delay90','delayqcv',
+#            'delaypctcount','delaypctmin','delaypctmax','delaypctmean','delaypctstd',
+#            'delaypct10','delaypct25','delaypct50','delaypct75','delaypct90','delaypctqcv']
+#tp.to_csv(path+'Output/Archive/ArchiveOutput.csv',index=False,header=True,mode='w')
 
 
 
@@ -144,13 +144,19 @@ for i in tp.index:
     else:
         start=sh[[x.split('.')[0]==tp.loc[i,'routeid'] for x in sh['shape_id']]].reset_index(drop=True)
     start['zip']=list(zip(start['shape_pt_lon'],start['shape_pt_lat']))
-    start=start[start['zip']==tp.loc[i,'startzip']]
+    if len(start)!=0:
+        start=start[start['zip']==tp.loc[i,'startzip']]
+    else:
+        start=pd.DataFrame(columns=['shape_id','shape_pt_lat','shape_pt_lon','shape_pt_sequence','zip'])
     if len(sh[sh['shape_id'].isin(pd.unique(trips[trips['route_id']==tp.loc[i,'routeid']]['shape_id']))])!=0:
         end=sh[sh['shape_id'].isin(pd.unique(trips[trips['route_id']==tp.loc[i,'routeid']]['shape_id']))].reset_index(drop=True)
     else:
         end=sh[[x.split('.')[0]==tp.loc[i,'routeid'] for x in sh['shape_id']]].reset_index(drop=True)
     end['zip']=list(zip(end['shape_pt_lon'],end['shape_pt_lat']))
-    end=end[end['zip']==tp.loc[i,'endzip']]    
+    if len(end)!=0:
+        end=end[end['zip']==tp.loc[i,'endzip']]
+    else:
+        end=pd.DataFrame(columns=['shape_id','shape_pt_lat','shape_pt_lon','shape_pt_sequence','zip'])
     startend=pd.merge(start,end,how='inner',on='shape_id')
     startend=startend[startend['shape_pt_sequence_y']>startend['shape_pt_sequence_x']].reset_index(drop=True)
     if len(startend)>0:
