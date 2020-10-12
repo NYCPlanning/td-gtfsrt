6a6fc91235dc098030c1fbb96d4db90c708b7bfc
Yijun Ma <mayijun1203@gmail.com>
2020-10-12 13:48:25 -0400

    Update siridownload.py

--- bus/siridownload.py
+++ bus/siridownload.py
@@ -16,50 +16,55 @@ url='http://bustime.mta.info/api/siri/vehicle-monitoring.json?key='+pd.read_csv(
 
 endtime=datetime.datetime(2020,12,31,23,0,0,0,pytz.timezone('US/Eastern'))
 while datetime.datetime.now(pytz.timezone('US/Eastern'))<endtime:
-    tp=pd.DataFrame(requests.get(url).json()).loc['ServiceDelivery','Siri']
-    ts=datetime.datetime.strptime(tp['ResponseTimestamp'].split('.')[0],'%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d_%H%M%S')
-    tp=tp['VehicleMonitoringDelivery'][0]['VehicleActivity']
-    veh=pd.DataFrame()
-    veh['veh']=''
-    veh['time']=''
-    veh['epoch']=np.nan
-    veh['lat']=np.nan
-    veh['long']=np.nan
-    veh['line']=''
-    veh['dir']=''
-    veh['dest']=''
-    veh['jrn']=''
-    veh['stpid']=''
-    veh['stpnm']=''
-    veh['dist']=np.nan
-    veh['pax']=np.nan
-    veh['cap']=np.nan
-    for j in range(0,len(tp)):
-        try:
-            if np.isin(tp[j]['MonitoredVehicleJourney']['ProgressRate'],['normalProgress','noProgress']):
-                veh.loc[j,'veh']=tp[j]['MonitoredVehicleJourney']['VehicleRef']
-                veh.loc[j,'time']=datetime.datetime.strptime(tp[j]['RecordedAtTime'].split('.')[0],'%Y-%m-%dT%H:%M:%S').strftime('%m%d%H%M%S')
-                veh.loc[j,'epoch']=pytz.timezone('US/Eastern').localize(datetime.datetime.strptime(tp[j]['RecordedAtTime'].split('.')[0],'%Y-%m-%dT%H:%M:%S')).timestamp()
-                veh.loc[j,'lat']=tp[j]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-                veh.loc[j,'long']=tp[j]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-                veh.loc[j,'line']=tp[j]['MonitoredVehicleJourney']['PublishedLineName']
-                veh.loc[j,'dir']=tp[j]['MonitoredVehicleJourney']['DirectionRef']
-                veh.loc[j,'dest']=tp[j]['MonitoredVehicleJourney']['DestinationName']
-                veh.loc[j,'jrn']=tp[j]['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
-                veh.loc[j,'stpid']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['StopPointRef']
-                veh.loc[j,'stpnm']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
-                veh.loc[j,'dist']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['CallDistanceAlongRoute']
-                try:
-                    veh.loc[j,'pax']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Capacities']['EstimatedPassengerCount']
-                    veh.loc[j,'cap']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Capacities']['EstimatedPassengerCapacity']
-                except:
-                    pass
-            else:
-                print(tp[j]['MonitoredVehicleJourney']['VehicleRef']+' no progress')
-        except:
-            print(tp[j]['MonitoredVehicleJourney']['VehicleRef']+' error')
-    veh.to_csv(path+'SIRI/Raw/rttp_'+ts+'.csv',index=False)
-    time.sleep(60)
+    try:
+        tp=pd.DataFrame(requests.get(url).json()).loc['ServiceDelivery','Siri']
+        ts=datetime.datetime.strptime(tp['ResponseTimestamp'].split('.')[0],'%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d_%H%M%S')
+        tp=tp['VehicleMonitoringDelivery'][0]['VehicleActivity']
+        veh=pd.DataFrame()
+        veh['veh']=''
+        veh['prog']=''
+        veh['time']=''
+        veh['epoch']=np.nan
+        veh['lat']=np.nan
+        veh['long']=np.nan
+        veh['line']=''
+        veh['dir']=''
+        veh['dest']=''
+        veh['jrn']=''
+        veh['stpid']=''
+        veh['stpnm']=''
+        veh['dist']=np.nan
+        veh['pax']=np.nan
+        veh['cap']=np.nan
+        for j in range(0,len(tp)):
+            try:
+                if np.isin(tp[j]['MonitoredVehicleJourney']['ProgressRate'],['normalProgress']):
+                    veh.loc[j,'veh']=tp[j]['MonitoredVehicleJourney']['VehicleRef']
+                    veh.loc[j,'prog']=tp[j]['MonitoredVehicleJourney']['ProgressRate']
+                    veh.loc[j,'time']=datetime.datetime.strptime(tp[j]['RecordedAtTime'].split('.')[0],'%Y-%m-%dT%H:%M:%S').strftime('%m%d%H%M%S')
+                    veh.loc[j,'epoch']=pytz.timezone('US/Eastern').localize(datetime.datetime.strptime(tp[j]['RecordedAtTime'].split('.')[0],'%Y-%m-%dT%H:%M:%S')).timestamp()
+                    veh.loc[j,'lat']=tp[j]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+                    veh.loc[j,'long']=tp[j]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+                    veh.loc[j,'line']=tp[j]['MonitoredVehicleJourney']['PublishedLineName']
+                    veh.loc[j,'dir']=tp[j]['MonitoredVehicleJourney']['DirectionRef']
+                    veh.loc[j,'dest']=tp[j]['MonitoredVehicleJourney']['DestinationName']
+                    veh.loc[j,'jrn']=tp[j]['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
+                    veh.loc[j,'stpid']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['StopPointRef']
+                    veh.loc[j,'stpnm']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
+                    veh.loc[j,'dist']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['CallDistanceAlongRoute']
+                    try:
+                        veh.loc[j,'pax']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Capacities']['EstimatedPassengerCount']
+                        veh.loc[j,'cap']=tp[j]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Capacities']['EstimatedPassengerCapacity']
+                    except:
+                        pass
+                else:
+                    print(tp[j]['MonitoredVehicleJourney']['VehicleRef']+' no progress')
+            except:
+                print(tp[j]['MonitoredVehicleJourney']['VehicleRef']+' error')
+        veh.to_csv(path+'SIRI/Raw/rttp_'+ts+'.csv',index=False)
+        time.sleep(60)
+    except:
+        print('request error')
 
 
 
