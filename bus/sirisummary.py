78f048f9cf6bf1567f41a9857619532eaea5ebee
Yijun Ma <mayijun1203@gmail.com>
2020-10-14 22:38:12 -0400

    Update sirisummary.py

--- bus/sirisummary.py
+++ bus/sirisummary.py
@@ -22,16 +22,7 @@ path='C:/Users/mayij/Desktop/DOC/DCP2019/GTFS-RT/Bus/'
 # Calculate schedule summary
 # sc=sts[0:8].reset_index(drop=True)
 def calcsc(sc):
-    sc=sc.reset_index(drop=True)
-    sc['stpid1']=sc['stpid'].copy()
-    sc['time1']=sc['time'].copy()
-    sc['sec1']=[pd.to_numeric(x.split(':')[0])*3600+pd.to_numeric(x.split(':')[1])*60+pd.to_numeric(x.split(':')[2]) for x in sc['time1']]
-    sc['stpid2']=np.roll(sc['stpid'],-1)
-    sc['time2']=np.roll(sc['time'],-1)
-    sc['sec2']=[pd.to_numeric(x.split(':')[0])*3600+pd.to_numeric(x.split(':')[1])*60+pd.to_numeric(x.split(':')[2]) for x in sc['time2']]
-    sc['scdur']=sc['sec2']-sc['sec1']
     sc=sc.iloc[:-1,:].reset_index(drop=True)
-    sc=sc[['jrn','stpid1','stpid2','scdur']].reset_index(drop=True)
     return sc
 
 
@@ -39,29 +30,7 @@ def calcsc(sc):
 # Calculate real time summary
 # rt=rttp[0:20].reset_index(drop=True)
 def calcrt(rt):
-    rt=rt.reset_index(drop=True)
-    rt['stpid1']=rt['stpid'].copy()
-    rt['stpnm1']=rt['stpnm'].copy()
-    rt['time1']=rt['time'].copy()
-    rt['epoch1']=rt['epoch'].copy()
-    rt['dist1']=rt['dist'].copy()
-    rt['pax1']=rt['pax'].copy()
-    rt['cap1']=rt['cap'].copy()
-    rt['stpid2']=np.roll(rt['stpid'],-1)
-    rt['stpnm2']=np.roll(rt['stpnm'],-1)
-    rt['time2']=np.roll(rt['time'],-1)
-    rt['epoch2']=np.roll(rt['epoch'],-1)
-    rt['dist2']=np.roll(rt['dist'],-1)
-    rt['pax2']=np.roll(rt['pax'],-1)
-    rt['cap2']=np.roll(rt['cap'],-1)
-    rt['dur']=rt['epoch2']-rt['epoch1']
-    rt['dist']=rt['dist2']-rt['dist1']
-    rt['mph']=(rt['dist']/1609)/(rt['dur']/3600)
-    rt['pax']=rt['pax2'].copy()
-    rt['cap']=rt['cap2'].copy()
     rt=rt.iloc[:-1,:].reset_index(drop=True)
-    rt=rt[['line','dir','dest','jrn','veh','stpid1','stpnm1','time1','epoch1','dist1',
-           'stpid2','stpnm2','time2','epoch2','dist2','dur','dist','mph','pax','cap']].reset_index(drop=True)
     return rt
 
 
@@ -76,7 +45,15 @@ def calcrt(rt):
 # sts['stpid']=sts['stop_id'].copy()
 # sts['time']=sts['arrival_time'].copy()
 # sts=sts[['jrn','stpid','time']].sort_values(['jrn','time'],ascending=True).drop_duplicates(keep='first').reset_index(drop=True)
+# sts['stpid1']=sts['stpid'].copy()
+# sts['time1']=sts['time'].copy()
+# sts['sec1']=[pd.to_numeric(x.split(':')[0])*3600+pd.to_numeric(x.split(':')[1])*60+pd.to_numeric(x.split(':')[2]) for x in sts['time1']]
+# sts['stpid2']=np.roll(sts['stpid'],-1)
+# sts['time2']=np.roll(sts['time'],-1)
+# sts['sec2']=[pd.to_numeric(x.split(':')[0])*3600+pd.to_numeric(x.split(':')[1])*60+pd.to_numeric(x.split(':')[2]) for x in sts['time2']]
+# sts['scdur']=sts['sec2']-sts['sec1']
 # sts=sts.groupby(['jrn'],as_index=False).apply(calcsc).reset_index(drop=True)
+# sts=sts[['jrn','stpid1','stpid2','scdur']].reset_index(drop=True)
 # sts.to_csv(path+'SIRI/Schedule/schedule.csv',index=False,header=True,mode='w')
 
 
@@ -108,22 +85,50 @@ def calcrt(rt):
 
 
 
-# # Summarize data by date
-# dates=sorted(pd.unique([x.split('_')[1] for x in os.listdir(path+'SIRI/Raw/') if x.startswith('rttp')]))
-# for d in dates:
-#     rttp=[]
-#     for i in sorted([x for x in os.listdir(path+'SIRI/Raw/') if x.startswith('rttp_'+str(d))]):
-#         print(str(i))
-#         rttp.append(pd.read_csv(path+'SIRI/Raw/'+str(i),dtype=str))
-#     rttp=pd.concat(rttp,axis=0,ignore_index=True)
-#     rttp['epoch']=pd.to_numeric(rttp['epoch'])
-#     rttp['dist']=pd.to_numeric(rttp['dist'])
-#     rttp['pax']=pd.to_numeric(rttp['pax'])
-#     rttp['cap']=pd.to_numeric(rttp['cap'])
-#     rttp=rttp.sort_values(['line','dir','dest','jrn','veh','epoch'],ascending=True).reset_index(drop=True)
-#     rttp=rttp.drop_duplicates(['line','dir','dest','jrn','veh','stpid'],keep='last').reset_index(drop=True)
-#     rttp=rttp.groupby(['line','dir','dest','jrn','veh'],as_index=False).apply(calcrt).reset_index(drop=True)
-#     rttp.to_csv(path+'SIRI/Output/tp_'+str(d)+'.csv',index=False,header=True,mode='w')
+# Summarize data by date
+sts=pd.read_csv(path+'SIRI/Schedule/schedule.csv',dtype=str,converters={'scdur':float})
+dates=sorted(pd.unique([x.split('_')[1] for x in os.listdir(path+'SIRI/Raw/') if x.startswith('rttp')]))
+for d in dates:
+    rttp=[]
+    for i in sorted([x for x in os.listdir(path+'SIRI/Raw/') if x.startswith('rttp_'+str(d))]):
+        print(str(i))
+        rttp.append(pd.read_csv(path+'SIRI/Raw/'+str(i),dtype=str))
+    rttp=pd.concat(rttp,axis=0,ignore_index=True)
+    rttp=rttp.dropna(axis=0,subset=['line','dir','dest','jrn','veh','stpid','epoch','dist']).reset_index(drop=True)
+    rttp['jrn']=['_'.join(x.split('_')[1:]) for x in rttp['jrn']]
+    rttp['stpid']=['_'.join(x.split('_')[1:]) for x in rttp['stpid']]
+    rttp['epoch']=pd.to_numeric(rttp['epoch'])
+    rttp['dist']=pd.to_numeric(rttp['dist'])
+    rttp['pax']=pd.to_numeric(rttp['pax'])
+    rttp['cap']=pd.to_numeric(rttp['cap'])
+    rttp=rttp.sort_values(['line','dir','dest','jrn','veh','epoch'],ascending=True).reset_index(drop=True)
+    rttp=rttp.drop_duplicates(['line','dir','dest','jrn','veh','stpid'],keep='last').reset_index(drop=True)
+    rttp['stpid1']=rttp['stpid'].copy()
+    rttp['stpnm1']=rttp['stpnm'].copy()
+    rttp['time1']=rttp['time'].copy()
+    rttp['epoch1']=rttp['epoch'].copy()
+    rttp['dist1']=rttp['dist'].copy()
+    rttp['pax1']=rttp['pax'].copy()
+    rttp['cap1']=rttp['cap'].copy()
+    rttp['stpid2']=np.roll(rttp['stpid'],-1)
+    rttp['stpnm2']=np.roll(rttp['stpnm'],-1)
+    rttp['time2']=np.roll(rttp['time'],-1)
+    rttp['epoch2']=np.roll(rttp['epoch'],-1)
+    rttp['dist2']=np.roll(rttp['dist'],-1)
+    rttp['pax2']=np.roll(rttp['pax'],-1)
+    rttp['cap2']=np.roll(rttp['cap'],-1)
+    rttp['wkd']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('US/Eastern')).weekday() for x in rttp['epoch1']]
+    rttp['hour']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('US/Eastern')).hour for x in rttp['epoch1']]
+    rttp['dur']=rttp['epoch2']-rttp['epoch1']
+    rttp['dist']=rttp['dist2']-rttp['dist1']
+    rttp['mph']=(rttp['dist']/1609)/(rttp['dur']/3600)
+    rttp['pax']=rttp['pax2'].copy()
+    rttp['cap']=rttp['cap2'].copy()
+    rttp=rttp.groupby(['line','dir','dest','jrn','veh'],as_index=False).apply(calcrt).reset_index(drop=True)
+    rttp=pd.merge(rttp,sts,how='left',on=['jrn','stpid1','stpid2'])
+    rttp=rttp[['line','dir','dest','jrn','veh','stpid1','stpnm1','time1','epoch1','dist1','stpid2','stpnm2',
+               'time2','epoch2','dist2','wkd','hour','dur','scdur','dist','mph','pax','cap']].reset_index(drop=True)
+    rttp.to_csv(path+'SIRI/Output/tp_'+str(d)+'.csv',index=False,header=True,mode='w')
 
 
 
@@ -140,47 +145,28 @@ def calcrt(rt):
 # Summarize all the data and calculate AM peak metrics
 tp=[]
 for i in sorted([x for x in os.listdir(path+'SIRI/Output/') if x.startswith('tp')]):
-    tp.append(pd.read_csv(path+'SIRI/Output/'+str(i),dtype=str))
+    tp.append(pd.read_csv(path+'SIRI/Output/'+str(i),dtype=str,converters={'dir':float,'epoch1':float,
+                                                                           'dist1':float,'epoch2':float,
+                                                                           'dist2':float,'wkd':float,
+                                                                           'hour':float,'dur':float,
+                                                                           'dist':float,'mph':float}))
 tp=pd.concat(tp,axis=0,ignore_index=True)
-tp=tp.dropna(axis=0,subset=['line','dir','dest','jrn','veh','stpid1','epoch1','dist1','stpid2','epoch2','dist2','dur','dist','mph']).reset_index(drop=True)
-tp['jrn']=['_'.join(x.split('_')[1:]) for x in tp['jrn']]
-tp['stpid1']=['_'.join(x.split('_')[1:]) for x in tp['stpid1']]
-tp['stpid2']=['_'.join(x.split('_')[1:]) for x in tp['stpid2']]
-tp['epoch1']=pd.to_numeric(tp['epoch1'])
-tp['dist1']=pd.to_numeric(tp['dist1'])
-tp['epoch2']=pd.to_numeric(tp['epoch2'])
-tp['dist2']=pd.to_numeric(tp['dist2'])
-tp['dur']=pd.to_numeric(tp['dur'])
-tp['dist']=pd.to_numeric(tp['dist'])
-tp['mph']=pd.to_numeric(tp['mph'])
+tp['scdur']=pd.to_numeric(tp['scdur'])
 tp['pax']=pd.to_numeric(tp['pax'])
 tp['cap']=pd.to_numeric(tp['cap'])
-tp['wkd']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('US/Eastern')).weekday() for x in tp['epoch1']]
-tp['hour']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('US/Eastern')).hour for x in tp['epoch1']]
 tp=tp[np.isin(tp['wkd'],[0,1,2,3,4])].reset_index(drop=True)
 tp=tp[np.isin(tp['hour'],[6,7,8,9])].reset_index(drop=True)
-sts=pd.read_csv(path+'SIRI/Schedule/schedule.csv',dtype=str)
-tp=pd.merge(tp,sts,how='left',on=['jrn','stpid1','stpid2'])
-
-269128
-156214
-
-
-tp=tp[['line','dir','dest','stpid1','stpid2','dur','dist','mph','pax','cap']].reset_index(drop=True)
-
-
-    tp['delay']=tp.duration-tp.schedule
-    tp['delaypct']=tp.duration/tp.schedule
-
-
-
-
-
-
-
+tp=tp[['line','dir','dest','stpid1','stpid2','dur','scdur','dist','mph','pax','cap']].reset_index(drop=True)
+tp['dly']=tp['dur']-tp['scdur']
+tp['dlypct']=tp['dly']/tp['scdur']
 
 k=tp.groupby(['line','dir','dest','stpid1','stpid2'],as_index=True).describe(percentiles=[0.1,0.25,0.5,0.75,0.9]).reset_index(drop=False)
 k.columns=[(x[0]+x[1]).replace('%','') for x in k.columns]
+
+
+
+
+
 
 
 tp['waittimeqcv']=(tp['waittime75']-tp['waittime25'])/tp['waittime50']
